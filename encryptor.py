754a34f7ef65b1278788c7eaeea4ec3d9109af7e
borkarra <borkarra@gmail.com>
2016-12-15 14:17:14 -0800

    Mastery Challenge
    Code submissions for mastery challenge

---
+++ encryptor.py
@@ -0,0 +1,12 @@
+def enc(msg):
+    c = []
+    for char in msg:
+        c.append(chr(ord(char)^0x69))
+    return ''.join(c)
+
+
+a = "hello world"
+b = "testing"
+print a,b
+print enc(a), enc(b)
+print enc(enc(a)), enc(enc(b))
